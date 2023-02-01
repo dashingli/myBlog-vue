43950feeb7ad8ecb003f8f25b6089fdfd4b30231
lilinzhe <1120294646@qq.com>
2023-02-01 21:26:38 +0800

    完成头像组件

---
+++ src/components/Avatar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img
+      :src="url"
+      class="avatar-img"
+      :style="{ width: width + 'px', height: width + 'px' }"
+    />
+  </div>
+</template>
+
+<script lang="js">
+export default {
+  props:{
+    url:{
+        type:String,
+        require:true,
+    },
+    width:{
+        type:Number,
+        default:150
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.avatar-img {
+  border-radius: 50%;
+  object-fit: cover;
+}
+</style>
