9ea0bf805995e83e1d83f108727e224a6e75fec3
lilinzhe <1120294646@qq.com>
2023-02-05 15:26:40 +0800

    Menu组件(未添加选中功能)

---
+++ src/components/Menu/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="menu-wrapper">
+    <a href="/">
+      <Icon name="home"></Icon>
+      首页
+    </a>
+    <a href="/blog">
+      <Icon name="blog"></Icon>
+      文章
+    </a>
+    <a href="/about">
+      <Icon name="about"></Icon>
+      关于我
+    </a>
+    <a href="/project">
+      <Icon name="code"></Icon>
+      项目&效果
+    </a>
+    <a href="/message">
+      <Icon name="chat"></Icon>
+      项目&效果
+    </a>
+  </div>
+</template>
+
+<script lang="js">
+import Icon from '@/components/Icon'
+import '@/style/reset.less'
+export default {
+    components:{
+        Icon
+    }
+}
+</script>
+
+<style lang="less" scoped>
+@import "~@/style/var.less";
+.menu-wrapper {
+  display: inline-flex;
+  flex-direction: column;
+  border-color: transparent;
+  a {
+    padding: 10px 40px 10px 20px;
+    color: @gray;
+    &:hover {
+      background-color: @gray;
+      color: white;
+    }
+  }
+}
+</style>
---
+++ src/components/Menu/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="test-wrapper">
+    <Menu></Menu>
+  </div>
+</template>
+
+<script lang="js">
+import Menu from '@/components/Menu'
+export default {
+    components:{
+        Menu
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.test-wrapper {
+  display: inline-block;
+  background-color: black;
+}
+</style>
