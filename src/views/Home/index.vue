edeec4aa9d70d94612295daecdad9d41fd1b14fe
lilinzhe <1120294646@qq.com>
2023-02-08 17:57:07 +0800

    完成鼠标滚动 | 上下箭头 | 点击导航圈 切换页面

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,21 +1,26 @@
 <template>
-  <div class="home-wrapper">
-    <div class="uparrow-wrapper" v-show="index > 1" @click="uparrowClick">
+  <div class="home-wrapper" ref="homeWrapper">
+    <div
+      class="uparrow-wrapper"
+      v-show="index > 1"
+      @click="indexChanged(index - 1)"
+    >
       <Icon name="arrowUp"></Icon>
     </div>
     <div
       class="arrowdown-wrapper"
       v-show="index < bannerList.length"
-      @click="arrowdownClick"
+      @click="indexChanged(index + 1)"
     >
       <Icon name="arrowDown"></Icon>
     </div>
     <div class="index-wrapper">
-      <ul>
+      <ul @transitionend="handleTransitionEnd">
         <li
           v-for="(banner, i) in bannerList"
           :key="banner.id"
           :class="{ active: i === index - 1 }"
+          @click="indexChanged(i + 1)"
         ></li>
       </ul>
     </div>
@@ -33,6 +38,13 @@ import Message from '@/components/Message'
 import {getBanner} from '@/api/banner.js'
 import Icon from '@/components/Icon'
 export default {
+  mounted(){
+    this.wrapperHeight = this.$refs.homeWrapper.clientHeight;
+    window.addEventListener('wheel',this.wheelChanged);
+  },
+  destroyed(){
+    window.removeEventListener('wheel',this.wheelChanged);
+  },
   data(){
       return {
         //Message组件相关
@@ -41,18 +53,34 @@ export default {
         mesType : 'success',//Messagea组件是什么类型的  'success' | 'error' | 'warn' | 'info'
         msgContent:'获取消息成功',//Message组件消息内容
         //banner API 返回的data数据
-        bannerList:null,
+        bannerList:[],
         //index 表示展示的是第几个数据
-        index:1
+        index:1,
+        //组件容器高度
+        wrapperHeight:0,
+        //是否正处于滚动中
+        isWheel : false,
       }
     },
     methods:{
-      uparrowClick(){
-        this.index = this.index -  1;
+      wheelChanged(event){
+        if(this.isWheel){
+          return;
+        }
+        if(event.deltaY < -5 && this.index >1){
+          this.isWheel = true;
+          this.index = this.index - 1;
+        }else if(event.deltaY > 5 && this.index < this.bannerList.length){
+          this.isWheel = true;
+          this.index = this.index + 1;
+        }
       },
-      arrowdownClick(){
-        this.index = this.index + 1;
-      }
+      handleTransitionEnd(){
+        this.isWheel = false;
+      },
+      indexChanged(val){
+        this.index = val
+      },
     },
   components:{
     Message,
@@ -100,8 +128,10 @@ export default {
     .active {
       background-color: @words;
       border: 1px solid #fff;
+      transition: background-color 500ms linear;
     }
   }
+  @animationDuration: 5000ms;
   .uparrow-wrapper {
     position: absolute;
     cursor: pointer;
@@ -110,7 +140,7 @@ export default {
     color: @gray;
     font-size: @iconFontSize;
     transform: translateX(-50%);
-    animation: uparrow 3000ms infinite;
+    animation: uparrow @animationDuration infinite;
   }
   @keyframes uparrow {
     0% {
@@ -123,6 +153,7 @@ export default {
       transform: translateY(10px);
     }
   }
+
   .arrowdown-wrapper {
     position: absolute;
     cursor: pointer;
@@ -131,7 +162,7 @@ export default {
     font-size: @iconFontSize;
     color: @gray;
     transform: translateX(-50%);
-    animation: arrowdown 3000ms infinite;
+    animation: arrowdown @animationDuration infinite;
   }
   @keyframes arrowdown {
     0% {
