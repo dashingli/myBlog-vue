514cad6cda8582ff5f99d8545e9b17909f86330d
lilinzhe <1120294646@qq.com>
2023-02-22 15:36:58 +0800

    完成滚动到底部加载评论功能

--- src/views/Blog/components/Detail/BlogContent/index.vue
+++ src/views/Blog/components/Detail/BlogContent/index.vue
@@ -20,10 +20,11 @@
       v-if="commentList.length !== 0"
       title="评论列表"
       id="commentList"
-      :commentTotal="commentNumber"
+      :commentTotal="commentTotal"
       :commentList="commentList"
       @submit="handleSubmit"
     ></MessageArea>
+    <img v-if="isLoading" src="../../../../../assets/loading.svg">
   </div>
 </template>
 
@@ -37,36 +38,63 @@ import deBounce from "@/utils/deBounce";
 export default {
   methods:{
     handleSubmit(comment){
-
-
       this.commentList.unshift(comment);
-      this.commentNumber++;
+      this.commentTotal++;
     },
     dateFormat,
     handleScroll(){
       this.$bus.$emit('mainScroll',this.$refs.container);
+    },
+   async getNewComments(){
+      if(this.page >= this.totalPages){
+        return;
+      }
+      this.isLoading = true;
+      this.page++
+      const res = await getComments(this.page,this.limit,this.$route.params.id);
+      const data = res.data;
+      this.commentList =  this.commentList.concat(data.rows);
+      this.commentTotal = data.total;
+      this.isLoading = false
+    },
+    scrollEnd(){
+      const dom = this.$refs.container
+      if(dom.scrollTop + dom.clientHeight === dom.scrollHeight){
+        this.getNewComments()
+      }else{
+        return
+      }
+    }
+  },
+  computed:{
+    totalPages(){
+     return  Math.ceil(this.commentTotal / this.limit)
     }
   },
   async created(){
     const res = await getComments(this.page,this.limit,this.$route.params.id);
     const data = res.data;
     this.commentList = data.rows;
-    this.commentNumber = data.total;
+    this.commentTotal = data.total;
+    window.getNewComments = this.getNewComments
   },
 
   mounted(){
-    const setDeBounce =  deBounce(this.handleScroll,50)
-    this.$refs.container.addEventListener('scroll',setDeBounce)
+    this.setDeBounce =  deBounce(this.handleScroll,50)
+    this.$refs.container.addEventListener('scroll',this.scrollEnd)
+    this.$refs.container.addEventListener('scroll',this.setDeBounce)
   },
   destroyed(){
-    this.$refs.container.removeEventListener('scroll',this.handleScroll);
+    this.$refs.container.removeEventListener('scroll',this.scrollEnd)
+    this.$refs.container.removeEventListener('scroll',this.setDeBounce);
   },
   data(){
     return {
       commentTotal:0,
       commentList:[],
       page:1,
-      limit:10
+      limit:10,
+      isLoading:false,
     }
   },
   components:{
@@ -89,6 +117,12 @@ export default {
   padding-bottom: 20px;
   padding-left: 20px;
   scroll-behavior: smooth;
+  position: relative;
+  img{
+    position: absolute;
+    left:50%;
+    transform: translateX(-50%);
+  }
   span {
     font-size: 12px;
     padding-right: 20px;
